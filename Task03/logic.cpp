ab16ee27ee4b6f5301fd0a486b953f5296f7d292
st <st@A508PC14>
2025-05-10 18:18:23 +0300

    Task03 is done!!!!

--- Task03/logic.cpp
+++ Task03/logic.cpp
@@ -8,6 +8,31 @@
 
 #include "logic.h"
 
+bool isPositiveMoreThenOther(int* raw, int m) {
+	int count = 0;
+
+	for (int j = 0; j < m; j++)
+	{
+		if (raw[j] > 0) {
+			count++;
+		}
+		return count > m - count;
+	}
+}
+
 int count_rows_with_more_positive_values(int** matrix, int n, int m) {
-	return 0;
+	if (matrix == nullptr || m <= 0 || n <= 0) {
+
+	}
+
+	int count = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		if (isPositiveMoreThenOther(matrix[i], m)) {
+			count++;
+		}
+	}
+
+	return count;
 }
